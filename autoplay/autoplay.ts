05477c2536e842a2ca67a192d5ccad4a17181eee
artemzuravlenko <artem.zhuravlenko@omneex.ru>
2022-10-12 01:20:25 +0300

    refactor

--- autoplay/autoplay.ts
+++ autoplay/autoplay.ts
@@ -3,6 +3,6 @@ import { View } from "../src/View";
 import { ControllerMVC } from "../src/ControllerMVC";
 import { Model } from "../src/Model";
 import {playRecordedGame} from "../src/controllers/playRecordedGame";
-import {immortalGame1851} from "../src/controllers/recordedGames/immortalGame1851";
+import {immortalGame1851} from "../src/controllers/playRecordedGame/recordedGames/immortalGame1851";
 
 new ControllerMVC(new Model(), new View(), playRecordedGame(immortalGame1851));
--- src/controllers/playRecordedGame/index.ts
+++ src/controllers/playRecordedGame/index.ts
@@ -1,5 +1,5 @@
 import {MoveCoordinates} from "./recordedGames/immortalGame1851";
-import {Model} from "../Model";
+import {Model} from "../../Model";
 
 export const playRecordedGame = (recordedGame: MoveCoordinates[], stepDelay: number = 1500) => (model: Model) => {
     recordedGame.forEach((move, i) => {
